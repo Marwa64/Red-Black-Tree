f702f4fff606d88995d5c9810923365f477e799c
Marwa64 <farabi.marwa@gmail.com>
2020-12-23 17:14:37 +0200

    initial structure

--- RedBlackTree.java
+++ RedBlackTree.java
@@ -1,30 +1,58 @@
 enum color{RED,BLACK};
 
-class Node{
-    int data;
-    Node left , right , parent;
+class Node<T>{
+    T data;
+    Node<T> left , right , parent;
     color clr;
 
-    Node(int data){
+    Node(T data){
         this.data=data;
         this.clr= color.RED;
         parent = right = left = null;
     }
 }
 
-public class RedBlackTree {
-    Node root;
+public class RedBlackTree<T> {
+    Node<T> root;
 
 
-
-
-    
-    public static void main(String[] args) {
-
+    public void insert() {
+    	
     }
-
-
-
-
+    public void delete(Node<T> node) {
+    	// Case 1
+    	if (node.right == null && node.left == null) {
+    		if (node.clr != node.parent.clr) {
+    			if (node.parent.clr == color.RED) {
+    				node.parent.clr = color.BLACK;
+    			}
+    			node = null;
+    		}
+    	}
+    	// Case 2
+    	if (node.right != null && node.left == null) {
+    		node = node.right;
+    		node.clr = color.BLACK;
+    		node.right = null;
+    	} else if (node.left != null && node.right == null) {
+    		node = node.left;
+    		node.clr = color.BLACK;
+    		node.left = null;
+    	}
+    	// Case 3
+    	
+    }
+    public void clear() {
+    	
+    }
+    public void print() {
+    	
+    }
+    public void fixViolation() {
+    	
+    }
+    public static void main(String[] args) {
+    	
+    }
 
 }

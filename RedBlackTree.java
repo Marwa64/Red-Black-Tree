7d69bb6de767cef69cc7e51690a98b19737e101d
samar <samareeyassin155@gmail.com>
2020-12-23 22:11:08 +0200

    added insert function

--- RedBlackTree.java
+++ RedBlackTree.java
@@ -1,6 +1,8 @@
+import java.awt.*;
+
 enum color{RED,BLACK};
 
-class Node<T>{
+class Node<T extends  Comparable<T> >{
     T data;
     Node<T> left , right , parent;
     color clr;
@@ -12,18 +14,103 @@ class Node<T>{
     }
 }
 
-public class RedBlackTree<T> {
+public class RedBlackTree<T extends Comparable<T>> {
     Node<T> root;
 
-    public void rotateRight() {
+    public void rotateRight(Node node) {
     	
     }
-    public void rotateLeft() {
+    public void rotateLeft(Node<T> node) {
     	
     }
-    public void insert() {
-    	
+    public void insert(Node<T> node) {
+        Node<T> current=this.root;
+
+        while (current!=null) {
+            if (current.data.compareTo(node.data) < 0) {
+                current=current.left;
+            }
+            else {
+                current=current.right;
+            }
+        }
+
+        node.parent=current;
+
+        if(current.data.compareTo(node.data)<0)
+            current.right=node;
+        else current.left=node;
+
+        node.left=node.right=null;
+        node.clr=color.RED;
+
+
+
+
+        //fixing violation;
+
+        Node<T> nGrandParent , nUncle;
+        while(node.parent.clr == color.RED){
+            nGrandParent=node.parent.parent;
+            if(node.parent == nGrandParent.left){
+                nUncle= nGrandParent.right;
+
+                if(nUncle.clr == color.RED){
+                    //recoloring
+                    node.parent.clr = color.BLACK;
+                    nUncle.clr= color.BLACK;
+                    nGrandParent.clr = color.RED;
+                    node=nGrandParent;
+                }
+                else {
+                    if(node == node.parent.right){
+                        node=node.parent;
+                        rotateLeft(node);
+                    }
+                    else {
+                        node.parent.clr = color.BLACK;
+                        nGrandParent.clr = color.RED;
+                        rotateRight(nGrandParent);
+                    }
+
+                }
+
+
+            }
+            else {
+                nUncle= nGrandParent.left;
+
+                if(nUncle.clr == color.RED){
+                    node.parent.clr = color.BLACK;
+                    nUncle.clr= color.BLACK;
+                    nGrandParent.clr = color.RED;
+                    node=nGrandParent;
+                }
+                else {
+                    if(node == node.parent.right){
+                        node=node.parent;
+                        rotateRight(node);
+                    }
+                    else {
+                        node.parent.clr = color.BLACK;
+                        nGrandParent.clr = color.RED;
+                        rotateLeft(nGrandParent);
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+
+
+        root.clr = color.BLACK;
+
     }
+
     public void search() {
     	
     }
